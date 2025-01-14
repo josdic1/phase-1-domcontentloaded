3fc888219e577d9687e4871de676d5e61a095709
josdic1 <joshuadicker@gmail.com>
2025-01-14 14:31:51 -0500

    module completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+   onLoad()
+})
+
+function onLoad() {
+  const p = document.querySelector('p')
+  p.textContent = 'This is really cool!'
+}
+
+
+
