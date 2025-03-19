67ce6f37e42d9ac8d16232e783cd9bf669e56932
Josh Dicker <joshdicker@joshs-mac-mini.mynetworksettings.com>
2025-03-19 14:15:58 -0400

    domcontentloaded done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+
+const init = () => {
+    const text = document.querySelector('p')
+    .textContent = 'This is really cool!'
+}
+
+window.addEventListener("DOMContentLoaded", init)
